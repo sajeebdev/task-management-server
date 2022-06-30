eac9167924f567b5fd8873a9d30a0570beab0591
sajeebdev <sajeebsarker5506@gmail.com>
2022-07-01 04:18:20 +0600

    update api done

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const cors = require('cors')
 const port = process.env.PORT || 5000 ;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-app.use(cors());
-app.use(express.json());
 
+// app.use(cors());
+app.use(express.json());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions))
 //task-management
 // 2wpYQGnjIvYsVp2w
-
-
 
 
 const uri = "mongodb+srv://task-management:2wpYQGnjIvYsVp2w@cluster0.iz29v.mongodb.net/?retryWrites=true&w=majority";
@@ -18,18 +23,56 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+;
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-//   client.close();
+async function run(){
+    try{
+        await client.connect();
+        const alltask =client.db("taskmanasment").collection("task");
+    //get my task
+        app.get('/task', async(req,res)=>{
+            const result = await alltask.find().toArray();
+            res.send(result);
 
-});
+        })
+    // post my task
+    app.post ('/addtask',async(req,res)=>{
+        const addtask = req.body;
+        const result = await alltask.insertOne(addtask);
+        res.send(result); 
+    })
+    //ubdate
+    app.patch("/updatetask/:id", async (req, res) => {
+        const id = req.params.id;
+       
+        const data = req.body;
+        const result = await alltask.updateOne(
+        {_id:ObjectId(id)}, // sometime _id:ObjectId(id)
+        {
+        $set: data,
+        }
+        );
+        res.send(result);
+        });
 
+    //delete 
 
+    app.delete("/task/:id", async (req, res) => {
+        const id = req.params.id;
+       
+        const result = await alltask.deleteOne({ _id: ObjectId(id) });
+        res.send(result);
+        });
+    }
+    
+    finally{
+
+}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! oo')
 })
 
 app.listen(port, () => {
